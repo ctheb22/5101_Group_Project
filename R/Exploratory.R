ae65083b234e20e60b1bca03a0288d4e24294c52
Chance Beachy <chance.beachy@gmail.com>
2025-10-02 10:30:31 -0500

    Update Exploratory.R
    Removed bee data code, replaced with initial classics code/comments

--- R/Exploratory.R
+++ R/Exploratory.R
@@ -1,86 +1,39 @@
 # Authors       : Chance Beachy, Clayton Stuhrenberg
 # Creation Date : 9/16/25
-# Purpose       : Initial/experimental exploration of the Bee data
+# Purpose       : Initial/experimental exploration of the data
 #-------------------------------------------------------------------------------
 
 #If needed, install tidyverse package using the comment below
 #install.packages("tidyverse")
 library(tidyverse)
 
-#If needed, install readxl package using the comment below
-#install.packages("readxl")
-library(readxl)
-
-
 #-------------------------------------------------------------------------------
-# Import Data Files
+# Import Data File
 #-------------------------------------------------------------------------------
 
-#'Adults and Brood' file:
+#'Classics.csv
 #'Data format:
-#'  Rows = multi-value indices (date, hive)
-#'  columns = different variable names
+#'  1007 rows (including a header row), each representing a different literary
+#'    work from the Gutenburg Project
 #'  
-#'  Each row describes a specific hive on a specific date, with each column 
-#'  containing a different variable value.
+#'  Many different columns broken into different subcatagories specified by the
+#'    leading words of the column name:
+#'  - "bibliography.[specific col]": columns concerning specific publication values
+#'      such as author, publish date, subject, language, etc.
+#'  - "metadata.[specific col]": columns relating to gutenburg project metadata such
+#'      as number of downloads and Gutenburg project popularity ranking
+#'  - "metrics.difficulty.[specific col]": columns containing difficulty ratings
+#'      calculated via many different specific standard indexes/methods
+#'  - "metrics.sentiments.[specific col]": columns attempting to numerically rate
+#'      each book based on public sentiment regarding the text such as polarity,
+#'      or subjectivity
+#'  - "metrics.statistics.[specific col]": columns containing specific calculated 
+#'      statistics for the given text such as "average sentance length", "letter
+#'      per word" and "characters" (total number of characters).
 #'
-#'4 sub sheets:
-#'  sheet 1: Adults 2021-22 
-#'    (Weirdly has duplicate columns, some random missing data values)
-#'  sheet 2: Brood 2021-22
-#'  sheet 3: Adults 2022-23
-#'  sheet 4: Brood 2022-23
+#'  Many of the columns have specific explanations about what they represent on the
+#'  data source site. The metrics columns specifically should be well understood
+#'  before they're used or referenced due to how abstract they can be.
+ 
 
-df_Adults <- read_excel("./Data/Direct/Adults and brood 2021-2022.xlsx",
-                       sheet = 1)
-
-df_Brood <- read_excel("./Data/Direct/Adults and brood 2021-2022.xlsx",
-                       sheet = 2)
-
-#'Continuous hive CO2':
-#'Data format:
-#'  Rows = timestamps every 5 minutes
-#'  Columns = Specific hive (and bee variety)
-#'    Likely needs column headings cleaned up or combined to import correctly.
-#'    
-#'  Forms a time series grid with each row containing a recorded CO2 value for
-#'  every column/hive at that timestamp
-#'  
-#'2 sub sheets:
-#'  sheet 1: 2021 experiment
-#'  sheet 2: 2022 experiment
-
-df_Co2 <- read_excel("./Data/Direct/Continuous hive CO2 2021-2022.xlsx",
-                       sheet = 1)
-
-#'Continuous hive temperature':
-#'Data format:
-#'  Rows = timestamps every 5 minutes
-#'  Column headings = Specific hive (and bee variety)
-#'    Likely needs column headings cleaned up or combined to import correctly.
-#'    
-#'  Forms a time series grid with each row containing a recorded temp value for
-#'  every column/hive at that timestamp
-#'  
-#'2 sub sheets:
-#'  sheet 1: 2021 experiment
-#'  sheet 2: 2022 experiment
-
-df_Temp <- read_excel("./Data/Direct/Continuous hive temperature 2021-2022.xlsx", 
-                       sheet = 1)
-
-#'Continuous hive weight':
-#'Data format:
-#'  Rows = timestamps every 5 minutes
-#'  Column headings = Specific hive (and bee variety)
-#'    Likely needs column headings cleaned up or combined to import correctly.
-#'    
-#'  Forms a time series grid with each row containing a recorded weight value for
-#'  every column/hive at that timestamp
-#'  
-#'2 sub sheets:
-#'  sheet 1: 2021 experiment
-#'  sheet 2: 2022 experiment
-
-df_Weight <- read_excel("./Data/Direct/Continuous hive weight 2021-2022.xlsx", 
-                       sheet = 1)
+df_classics <- read_csv("./Data/classics.csv")
